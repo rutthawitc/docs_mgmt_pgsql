96af262ac8220954320d25e747392676e8e4c1e4
Rutthawit <rutthawit@gmail.com>
2020-06-01 11:31:02 +0700

    --Modify setting.json

--- docsmgmt_project/docsmgmt/views.py
+++ docsmgmt_project/docsmgmt/views.py
@@ -14,23 +14,23 @@ from django.views.generic import ListView, CreateView, UpdateView
 #from django.urls import reverse_lazy
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
-#Paginator
+# Paginator
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 @login_required
 def Home(request):
-    #show document counter on home page.
-    #readed
+    # show document counter on home page.
+    # readed
     readed_docs = Accepted.objects.filter(
         Q(is_accepted=True) &
-        Q(user__user_id=request.user.profile.id)).values_list('doc_no',flat=True)
+        Q(user__user_id=request.user.profile.id)).values_list('doc_no', flat=True)
 
     #readed_count = readed_docs.count()
 
-    #----------Check Role QuerySet---------
-    #----Edited on --25-May-2020----
+    # ----------Check Role QuerySet---------
+    # ----Edited on --25-May-2020----
     if (request.user.profile.role == 1):
         unread_list = Documents.objects.filter(
             Q(role=1) |
@@ -46,28 +46,30 @@ def Home(request):
     unread_count = unread_list.count()
 
     #context = {'unread_count':unread_count, 'readed_count':readed_count}
-    context = {'unread_count':unread_count}
-    #print(context)
+    context = {'unread_count': unread_count}
+    # print(context)
     return render(request, 'docsmgmt/home.html', context)
+
 
 @login_required
 def ShowAllDocuments(request):
     docs = Documents.objects.all()
-    error ="ไม่มีเอกสารใหม่"
-    context = {'docs':docs, 'error':error}
+    error = "ไม่มีเอกสารใหม่"
+    context = {'docs': docs, 'error': error}
     return render(request, 'docsmgmt/alldocs.html', context)
 
+
 @login_required
-#Show Unreaded Documents filter by Current User ID! 
+# Show Unreaded Documents filter by Current User ID!
 def ShowUnreadDocs(request):
-    #Find Readed (Accepted Documents) - QuerySet
+    # Find Readed (Accepted Documents) - QuerySet
     readed_docs = Accepted.objects.filter(
         Q(is_accepted=True) &
         Q(user__user_id=request.user.profile.id)
-    ).values_list('doc_no',flat=True)
+    ).values_list('doc_no', flat=True)
 
-#----------Check Role QuerySet---------
-#----Edited on --25-May-2020----
+# ----------Check Role QuerySet---------
+# ----Edited on --25-May-2020----
     if (request.user.profile.role == 1):
         unread_list = Documents.objects.filter(
             Q(role=1) |
@@ -80,15 +82,15 @@ def ShowUnreadDocs(request):
             Q(doc_dept=request.user.profile.dept)
         ).exclude(id__in=readed_docs).order_by('-id')
 
-        docscount = unread_list.count() 
-#----------End Query----
+        docscount = unread_list.count()
+# ----------End Query----
 
-    #DEBUG
+    # DEBUG
     #print('User Role', request.user.profile.role)
-    #print('unread_role',unread_list.values_list())
+    # print('unread_role',unread_list.values_list())
     #print('User dept',request.user.profile.dept)
 
-    #Paginator
+    # Paginator
     page = request.GET.get('page', 1)
     paginator = Paginator(unread_list, 15)
     try:
@@ -98,20 +100,21 @@ def ShowUnreadDocs(request):
     except EmptyPage:
         docs = paginator.page(paginator.num_pages)
 
-    error ="ไม่มีเอกสารใหม่"
-    context = {'error':error, 'docs':docs, 'docscount':docscount }
+    error = "ไม่มีเอกสารใหม่"
+    context = {'error': error, 'docs': docs, 'docscount': docscount}
     return render(request, 'docsmgmt/unread.html', context)
 
+
 @login_required
-#Show Accepted Documents filter by User ID! 
+# Show Accepted Documents filter by User ID!
 def ShowAcceptedDocs(request):
-    #Find Readed - QuerySet
+    # Find Readed - QuerySet
     accepted_list = Accepted.objects.filter(
         Q(is_accepted=True) &
         Q(user__user_id=request.user.profile.id)
     ).order_by('-accepted_date')
     acceptedcount = accepted_list.count()
-    #Paginator
+    # Paginator
     page = request.GET.get('page', 1)
     paginator = Paginator(accepted_list, 15)
     try:
@@ -121,25 +124,26 @@ def ShowAcceptedDocs(request):
     except EmptyPage:
         docs = paginator.page(paginator.num_pages)
 
-    error ={"No data!"}
-    context = {'error':error, 'docs':docs, 'acceptedcount':acceptedcount }
+    error = {"No data!"}
+    context = {'error': error, 'docs': docs, 'acceptedcount': acceptedcount}
     return render(request, 'docsmgmt/showaccepted.html', context)
+
 
 @login_required
 def ShowDocsByDept(request):
-    #Complete -QuerySet-
+    # Complete -QuerySet-
     docs_list = Documents.objects.filter(
         Q(doc_dept=request.user.profile.dept) |
         Q(doc_dept__id=3)
     ).order_by('-doc_dept__id', 'upload_date')
     docscount = docs_list.count()
-    #Paginator
+    # Paginator
     page = request.GET.get('page', 1)
     paginator = Paginator(docs_list, 15)
-    #-----DEBUG-------
-    #print(paginator.count)
-    #print(paginator.num_pages)
-    #print(paginator.page_range)
+    # -----DEBUG-------
+    # print(paginator.count)
+    # print(paginator.num_pages)
+    # print(paginator.page_range)
     try:
         docs = paginator.page(page)
     except PageNotAnInteger:
@@ -147,60 +151,66 @@ def ShowDocsByDept(request):
     except EmptyPage:
         docs = paginator.page(paginator.num_pages)
 
-    error ={"No data!"}
-    context = {'docs':docs, 'error':error, 'docscount':docscount}
+    error = {"No data!"}
+    context = {'docs': docs, 'error': error, 'docscount': docscount}
     return render(request, 'docsmgmt/docsbydept.html', context)
+
 
 @login_required
 def DocAccepted(request):
     data = json.loads(request.body)
     documentId = data['documentId']
     action = data['action']
-    #--DEBUG---
+    # --DEBUG---
     #print('Document Id:', documentId)
     #print('Action:', action)
 
-    #Set values
+    # Set values
     user = request.user.profile
     document = Documents.objects.get(id=documentId)
 
-    accepted = Accepted.objects.create(user=user, doc_no=document, is_accepted=True)
+    accepted = Accepted.objects.create(
+        user=user, doc_no=document, is_accepted=True)
     accepted.save()
-    #--DEBUG---
+    # --DEBUG---
     #print('User:', user)
     #print('Document ID:', document)
 
     return JsonResponse('Accepted', safe=False)
 
+
 @login_required
 def DocDetail(request, doc_pk):
     doc = Documents.objects.get(id=doc_pk)
     comments = Comments.objects.filter(doc_no=doc.id)
-    usercount = Accepted.objects.filter(doc_no=doc_pk, user__dept=request.user.profile.dept, is_accepted=True).count()
+    usercount = Accepted.objects.filter(
+        doc_no=doc_pk, user__dept=request.user.profile.dept, is_accepted=True).count()
     try:
         isaccepted = Accepted.objects.get(doc_no=doc.id, user=request.user.id)
     except Accepted.DoesNotExist:
         isaccepted = None
 
-    context = {'doc':doc, 'isaccepted':isaccepted, 'comments':comments, 'usercount':usercount}
+    context = {'doc': doc, 'isaccepted': isaccepted,
+               'comments': comments, 'usercount': usercount}
     return render(request, 'docsmgmt/docdetail.html', context)
+
 
 def getcomment(request):
     data = json.loads(request.body)
     documentId = data['documentId']
     action = data['action']
     text = data['memo']
-    #--DEBUG---
+    # --DEBUG---
     #print('Document Id:', documentId)
     #print('Action:', action)
     #print('text:', text)
 
-    #Set values
+    # Set values
     user = request.user.profile
     document = Documents.objects.get(id=documentId)
 
-    #DEBUG
-    #print(user)
+    # DEBUG
+    # print(user)
     #print('Doc ID :',document.id)
     #print('text:', text)
     comment = Comments.objects.create(user=user, doc_no=document, comment=text)
@@ -208,43 +218,46 @@ def getcomment(request):
 
     return JsonResponse('Commented', safe=False)
 
+
 def loginuser(request):
     username_value = ''
     password_vlaue = ''
     if request.method == 'GET':
-        return render(request, 'docsmgmt/login.html', {'form':AuthenticationForm()})
+        return render(request, 'docsmgmt/login.html', {'form': AuthenticationForm()})
     else:
-        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
-        
-        #DEBUG
-        #print(request.POST['username'])
-        #print(request.POST['password'])
-        #print(user)
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+
+        # DEBUG
+        # print(request.POST['username'])
+        # print(request.POST['password'])
+        # print(user)
         if user is None:
-            return render(request, 'docsmgmt/login.html', {'form':AuthenticationForm(), 'error':'Username and password did not match'})
+            return render(request, 'docsmgmt/login.html', {'form': AuthenticationForm(), 'error': 'Username and password did not match'})
         else:
-           login(request, user)
-           return redirect('home')
+            login(request, user)
+            return redirect('home')
 
 
 @login_required
 def logoutuser(request):
-    #if request.method == 'POST':
-        logout(request)
-        return HttpResponseRedirect('/login/')
+    # if request.method == 'POST':
+    logout(request)
+    return HttpResponseRedirect('/login/')
 
-#Change password
+# Change password
 @login_required
 def change_password(request):
-    msg=''
+    msg = ''
     if request.method == 'POST':
         form = PasswordChangeForm(request.user, request.POST)
         if form.is_valid():
             user = form.save()
             update_session_auth_hash(request, user)  # Important!
-            messages.success(request, 'Your password was successfully updated!')
+            messages.success(
+                request, 'Your password was successfully updated!')
             msg = 'Your password was successfully updated!'
-            #return redirect('change_password')
+            # return redirect('change_password')
         else:
             messages.error(request, 'Please correct the error below.')
             msg = 'Please correct the error below.'
@@ -252,10 +265,11 @@ def change_password(request):
         form = PasswordChangeForm(request.user)
     return render(request, 'accounts/change_password.html', {
         'form': form,
-        'msg':msg,
+        'msg': msg,
     })
 
-@login_required #User must login first
+
+@login_required  # User must login first
 def searchdocs(request):
     if request.method == 'POST':
         data = request.POST.copy()
@@ -269,9 +283,10 @@ def searchdocs(request):
                 Q(doc_mtno__contains=search_title) &
                 Q(doc_dept=request.user.profile.dept)
             ).order_by('-id')
-            #print(request.user.profile.dept)
+            # print(request.user.profile.dept)
             rescount = res.count()
-            context = {'res': res, 'src': 'founded', 'rescount':rescount, 'search_title':search_title}
+            context = {'res': res, 'src': 'founded',
+                       'rescount': rescount, 'search_title': search_title}
         except Documents.DoesNotExist:
             context = {'res': 'Has no resualt', 'src': 'notfound'}
             #res = 'Has no resualt'
@@ -281,38 +296,23 @@ def searchdocs(request):
     return render(request, 'docsmgmt/searchdoc.html')
 
 
-#List Users accepted --> Doc
+# List Users accepted --> Doc
 @login_required
 def listusers_accepted(request, doc_pk):
-    context ={}
+    context = {}
     doc = Documents.objects.get(id=doc_pk)
-    user_list = Accepted.objects.filter(doc_no=doc_pk, user__dept=request.user.profile.dept, is_accepted=True).order_by('accepted_date')
+    user_list = Accepted.objects.filter(
+        doc_no=doc_pk, user__dept=request.user.profile.dept, is_accepted=True).order_by('accepted_date')
     usercount = user_list.count()
 
-    #DEBUG
-    #print(user_list)
-    #print(usercount)
-    context = {'user_list':user_list, 'usercount':usercount, 'doc':doc}
+    # DEBUG
+    # print(user_list)
+    # print(usercount)
+    context = {'user_list': user_list, 'usercount': usercount, 'doc': doc}
     return render(request, 'docsmgmt/listaccepted.html', context)
 
-
-
-
-
-
-#class DocumentView(CreateView):
+# class DocumentView(CreateView):
 #    model = Documents
 #    fields = ('type_code', 'doc_mtno', 'doc_title', 'doc_desc', 'doc_date', 'doc_dept', 'doc_file')
 #    success_url = reverse_lazy('home')
-
-
-
-
-
-
-
-
-    
-
-
 
